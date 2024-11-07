668a345aa3aab702ab716824fed34c84c4f8d660
Yajaira Pierrez <yajairapc376@gmail.com>
2024-11-07 15:15:31 -0600

    Added package.json

--- hello.js
+++ hello.js
@@ -1,9 +1,24 @@
 var http= require('http');
+var fs= require('fs');
 var url= require('url');
 
 http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type': 'text/html'});
-	var q= url.parse(req,url,true).query;
-	var dates= q.year+ " " + q.month;
-	res.end(dates);
+	var q= url.parse(req.url,true);
+	var filename="." + q.pathname; 
+	if (filename=='./') {
+		filename='./index';}
+
+		filename=filename + ".html";
+		console.log(filename);
+		fs.readFile(filename,function(err,dat){
+			if(err){
+				res.writeHead(404,{'Content-Type':'text/html'});
+				return res.end("404 not found");
+			}
+			res.writeHead(200,{'Content-Type':'text/html'});
+			res.write(data);
+			return res.end();
+		});
 }).listen(8080);
+
+console.log("server listening on port 8000...")
